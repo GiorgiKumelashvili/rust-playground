a5f71c2c8dcc05fb35aae31565e193a1c92c0082
Gio <giorgi.kumelashvili21@gmail.com>
2025-12-17 00:01:04 +0400

    added assets

--- examples/gen_fig_font.rs
+++ examples/gen_fig_font.rs
@@ -1,7 +1,7 @@
 use figlet_rs::FIGfont;
 
 fn main() {
-    let small_font = FIGfont::from_file("fonts/Cosmike.flf").unwrap();
+    let small_font = FIGfont::from_file("assets/fonts/Cosmike.flf").unwrap();
     let figure = small_font.convert("Hello Rust");
     println!("{}", figure.unwrap());
 }
